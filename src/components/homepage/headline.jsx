d856fb11c8760721fb6e42173254160c7aa3ca9c
Dionisius951 <dionisius951@gmail.com>
2023-11-20 13:29:19 +0700

    menambahkan input bar dan button beserta stylingnya

--- src/components/homepage/headline.jsx
+++ src/components/homepage/headline.jsx
@@ -1,6 +1,10 @@
 import React from "react";
 import Box from "@mui/material/Box";
-import { TextField, Typography } from "@mui/material";
+import { Typography } from "@mui/material";
+import SearchIcon from "@mui/icons-material/Search";
+import Button from "@mui/material/Button";
+// @ts-ignore
+import Style from "./headline.module.css";
 
 export default function Headline() {
   return (
@@ -11,11 +15,11 @@ export default function Headline() {
         justifyContent: "center",
         alignItems: "center",
         height: "100%",
-        textAlign : "left",
-        p:2,
+        textAlign: "left",
+        p: 3,
       }}
     >
-      <Box sx={{ display: "flex",width:"100%",}}>
+      <Box sx={{ display: "flex", width: "100%" }}>
         <Typography
           variant="h5"
           sx={{
@@ -23,7 +27,6 @@ export default function Headline() {
             fontWeight: 700,
             fontSize: "64px",
             textTransform: "capitalize",
-            
           }}
         >
           Skill
@@ -40,13 +43,31 @@ export default function Headline() {
           Mastery
         </Typography>
       </Box>
-      <Typography sx={{
-        fontSize:"24px"
-      }}>
+      <Typography
+        sx={{
+          fontSize: "24px",
+        }}
+      >
         Media belajar online yang menginspirasi, menghadirkan peluang untuk
         meningkatkan keahlian Anda dalam berbagai bidang
       </Typography>
-      
+
+      <div className={`${Style.input_wrapper}`}>
+        <div className={`${Style.input_bar}`}>
+          <SearchIcon className={`${Style.icon}`} />
+          <input type="text" placeholder="Search...." />
+        </div>
+          <Button variant="contained" sx={{
+            backgroundColor : "#0b5ed7",
+            fontFamily : "inherit",
+            color : "white",
+            fontSize : "20px",
+            width : "200px",
+            fontWeight : 400,
+            textTransform : "capitalize",
+            px : 2,
+          }}>Cari Course</Button>
+      </div>
     </Box>
   );
 }
