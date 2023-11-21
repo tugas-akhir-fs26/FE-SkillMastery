297371e5b26696843c93b1fa45610f1ad0469755
Dionisius951 <dionisius951@gmail.com>
2023-11-21 11:51:44 +0700

    menambah section testimoni

---
+++ src/components/homepage/testimoni.jsx
@@ -0,0 +1,27 @@
+import Box from "@mui/material/Box";
+import Typography from "@mui/material/Typography";
+import React from "react";
+
+export default function Testimoni() {
+  return (
+    <>
+      <Box sx={{ textAlign: "center" }}>
+        <div className="Header">
+          <Typography variant="h3" sx={{ fontWeight: 600, color: "#0460D9" }}>
+            Testimoni
+          </Typography>
+          <Typography
+            variant="h5"
+            sx={{ fontWeight: 400, display: "flex", justifyContent: "center" }}
+          >
+            Apa kata mereka pengguna
+            <Typography variant="h5" sx={{marginLeft: "5px", fontWeight: 600, color:"#0460D9"}}>
+              Skill
+            </Typography>
+            <Typography variant="h5" sx={{fontWeight: 600}}>Mastery</Typography> ?
+          </Typography>
+        </div>
+      </Box>
+    </>
+  );
+}
