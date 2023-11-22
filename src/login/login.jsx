802a238c597b34b5d0a95b6a7bb20080135394dc
callmendre <andridaniata@gmail.com>
2023-11-22 09:32:43 +0700

    login ui + responsif done

---
+++ src/login/login.jsx
@@ -0,0 +1,56 @@
+import React, { useState } from "react";
+import "./style.css"
+function Login(){
+
+  const [email, setEmail] = useState()
+  const [password, setPassword] = useState()
+  const handleLogin = () =>{
+    if(email && password !== ''){
+      alert('Login Successful')
+    }
+  }
+
+  return(
+    <>
+      <div className=" row g-0 justify-content-center align-items-center" id="body-login">
+      
+        <div className="col-10 row g-0 align-items-center">
+
+          <div className="col-6 bg-primary mt-5" id="gambar-login">
+
+            <img src="https://skillindiacsc.in/wp-content/uploads/2022/06/Illustration-4.png" alt="" />
+          </div>
+
+        
+
+        <form action="" className="col-6 py-4 px-3" id="form-login">
+          
+          <center><h4 className="login-title">Login</h4></center>
+
+          <div className="form-floating mb-3">
+            <input type="email" className='form-control has-validation' id="email" placeholder="name@example.com" onChange={(e)=>{setEmail(e.target.value)}} required/>
+            <label htmlFor="email">Email</label>
+            <div class="invalid-feedback">
+              Email tidak boleh kosong
+            </div>
+          </div>
+          <div className="form-floating mb-3">
+            <input type="password" className='form-control' id="password" placeholder="password" onChange={(e) =>{setPassword(e.target.value)}} required/>
+            <label htmlFor="password">Password</label>
+          </div>
+          <div className="text-center" onClick={()=>{handleLogin()}}>
+            <button className="btn btn-outline-primary">
+              Login
+            </button>
+          </div>
+                  <div className="text-center mt-3">
+          Belum punya akun? <a href="">Daftar</a>
+        </div>
+        </form>
+      </div>
+      </div>
+    </>
+  )
+}
+
+export default Login;
