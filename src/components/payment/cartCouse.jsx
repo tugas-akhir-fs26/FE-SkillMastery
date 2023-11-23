48d06aa6a8cd4c6bdc3fb02b38345838c5902280
Dionisius951 <dionisius951@gmail.com>
2023-11-24 00:00:53 +0700

    responsive list course cart section

--- src/components/payment/cartCouse.jsx
+++ src/components/payment/cartCouse.jsx
@@ -1,38 +1,29 @@
+// @ts-nocheck
 import { Avatar, Box, Typography } from "@mui/material";
 import React from "react";
+import Style from "./cartCourse.module.css"
 
 export default function CartCourse() {
   return (
-    <Box sx={{ width : "60vw",display: "flex", alignItems: "center", justifyContent : "space-around", p:3, borderTop : "1px solid black" }}>
-      <img src="../src/assets\courses\web.jpg" alt="web" width={250} />
+    <Box sx={{ width : {xs : "300px", md : "60vw"}, display: "flex", flexDirection : {xs : "column", md : "row"}, alignItems: "center", justifyContent : "space-around", p:3, borderTop : "1px solid black" }}>
+      <img src="../src/assets/courses/web.jpg" alt="web" className={`${Style.img_cart}`} />
       <Box>
-        <Typography sx={{ fontWeight: 600, fontSize: "24px", marginBottom : "16px" }}>
+        <Typography sx={{ fontWeight: 600, fontSize: {xs : "20px" ,md : "24px"}, marginBottom : "16px" }}>
           Belajar NodeJS
         </Typography>
-        <Box sx={{ display: "flex", alignItems: "center", gap: "12px" }}>
+        <Box sx={{ display: "flex", alignItems: "center", gap: {xs : "8px",md : "12px"} }}>
           <Avatar
             alt="Remy Sharp"
             src="/static/images/avatar/2.jpg"
-            sx={{ width: "25px", height: "25px" }}
+            sx={{ width:  {xs : "20px" ,md : "25px"}, height: {xs : "20px" ,md : "25px" }}}
           />
-          <Typography sx={{ fontWeight: 400, fontSize: "14px" }}>
+          <Typography sx={{ fontWeight: {xs : 600, md : 400}, fontSize: {xs : "10px", md : "14px"} }}>
             Dionisius Reinaldo
-          </Typography>
-        </Box>
-        <Box sx={{display : "flex", justifyContent : "space-between", width : "300px", marginTop : "10px"}}>
-          <Typography sx={{ fontSize: "14px", fontWeight: 300 }}>
-            20 Jam Total Durasi
-          </Typography>
-          <Typography sx={{ fontSize: "14px", fontWeight: 300 }}>
-            * 6 Bab
-          </Typography>
-          <Typography sx={{ fontSize: "14px", fontWeight: 300 }}>
-            * Semua Level
           </Typography>
         </Box>
       </Box>
       <Box>
-        <Typography variant="h5" sx={{fontWeight : 600}}>
+        <Typography sx={{fontWeight : 600, fontSize : {xs : "18px" ,md : "24px"}, marginTop : {xs : "18px", md : 0}}}>
             Rp99.0000
         </Typography>
       </Box>
--- src/pages/payment.jsx
+++ src/pages/payment.jsx
@@ -1,29 +1,38 @@
 import React from "react";
 import CartCourse from "../components/payment/cartCouse";
 import Typography from "@mui/material/Typography";
-import { Box } from "@mui/material";
+import { Box, Grid } from "@mui/material";
 import Subtotal from "../components/payment/subtotal";
 
 function Payment() {
   return (
-    <Box sx={{ p: 3, display: "flex", alignItems : "start", gap : "18px"}}>
-      <Box>
-        <Typography
-          variant="h5"
-          sx={{ fontWeight: 800, fontSize: "32px", p: 2 }}
-        >
-          Shopping Cart
-        </Typography>
+    <Box sx={{ p: 3, display: "flex", alignItems: "start", gap: "18px" }}>
+      <Grid container spacing={2}>
+        <Grid item xs={12} md={8}>
+          <Box>
+            <Typography
+              variant="h5"
+              sx={{ fontWeight: 800, fontSize: "32px", p: 2 }}
+            >
+              Shopping Cart
+            </Typography>
 
-        <Typography sx={{ fontWeight: 300, fontSize: "18px", p: 1 }}>
-          2 Course in cart
-        </Typography>
-        <CartCourse />
-        <CartCourse />
-      </Box>
-      <Box sx={{width : "30vw", p:2, height :"50vh", marginTop : "48px"}}>
-        <Subtotal />
-      </Box>
+            <Typography sx={{ fontWeight: 300, fontSize: "18px", p: 1 }}>
+              2 Course in cart
+            </Typography>
+
+            {/* render list cart course */}
+            <CartCourse />
+            <CartCourse />
+          </Box>
+        </Grid>
+        <Grid item xs={12} md={4}>
+          <Box sx={{ width: "30vw", p: 2, height: "50vh", marginTop: "48px" }}>
+            {/* render subtotal */}
+            <Subtotal />
+          </Box>
+        </Grid>
+      </Grid>
     </Box>
   );
 }
