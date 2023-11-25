ce6bcd12ffa09623effb563205e0f0c205a2c38a
Dionisius951 <dionisius951@gmail.com>
2023-11-25 23:43:01 +0700

    responsive footer

--- src/components/footer/footer.jsx
+++ src/components/footer/footer.jsx
@@ -3,6 +3,7 @@ import React from "react";
 import Style from "./footer.module.css"
 // import { SvgIcon } from "@mui/material";
 import SvgIcons from "./iconsvg";
+import LinkSection from "./link";
 
 export default function Footer() {
   return (
@@ -17,32 +18,7 @@ export default function Footer() {
             </div>
           </div>
           <SvgIcons />
-          <div className="col-md-4 align-items-center d-flex justify-content-center">
-            <div className={`${Style.footer_link}`}>
-              <ul>
-                <li>
-                  <a href="">Kategori</a>
-                </li>
-                <li>
-                  <a href="">Testimoni</a>
-                </li>
-                <li>
-                  <a href="">Workshop</a>
-                </li>
-              </ul>
-              <ul>
-                <li>
-                  <a href="">Tentang Kami</a>
-                </li>
-                <li>
-                  <a href="">Hubungi Kami</a>
-                </li>
-                <li>
-                  <a href="">Bantuan</a>
-                </li>
-              </ul>
-            </div>
-          </div>
+          <LinkSection />
         </div>
       </div>
     </footer>
