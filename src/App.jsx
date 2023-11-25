b942b68b08d215d57b3e3fd7cd4929c0b97faad9
Dionisius951 <dionisius951@gmail.com>
2023-11-25 18:34:24 +0700

    menambahkan footer on progress

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,7 @@ import Workshop from './pages/workshop'
 import Payment from './pages/payment'
 import { createTheme, ThemeProvider } from '@mui/material'
 import './app.css'
+import Footer from './components/footer/footer'
 
 const theme = createTheme({
   typography : {
@@ -30,6 +31,7 @@ function App() {
       <Route path="/workshop" element={<Workshop />} />
       <Route path="/payment" element={<Payment />} />
     </Routes>
+    <Footer />
     </ThemeProvider>
   )
 }
