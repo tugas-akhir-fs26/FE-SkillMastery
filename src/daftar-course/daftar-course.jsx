2d9c60b70626bde820a929ef90446f0c0b7f5233
callmendre <andridaniata@gmail.com>
2023-11-25 13:18:33 +0700

    daftar course, dkk done

---
+++ src/daftar-course/daftar-course.jsx
@@ -0,0 +1,294 @@
+import React from "react";
+import "./style.css"
+
+function DaftarCourse(){
+    return(
+        <div>
+            {/*navbar*/}
+            <div className="container">
+                <h1>Temukan wawasan baru di <b>Skill<span className="mastery">Mastery</span></b></h1>
+                <p>Temukan Wawasan Baru: Pilih kursus yang sesuai dengan minat dan ambisi Anda <br /> untuk mengejar pengetahuan baru.</p>
+                <form class="d-flex" >
+                    <input class="form-control me-2 search" type="search" placeholder="Cari Kelas.." aria-label="Search"/>
+                    <button class="btn btn-success "  type="submit">Cari</button>
+                </form>
+
+            </div>
+
+            <div className="container mt-3 head-filter">
+                    <div className="col ">
+                        <a href="" className="filter">Pemrograman Web</a>
+                        <a href="" className="filter">Bahasa Pemrograman</a>
+                        <a href="" className="filter">Desain Grafis</a>
+                        <a href="" className="filter">Jaringan Komputer</a>
+                    </div>
+            </div>
+
+
+            <div className="container">
+                    <div className="row gy-3 mt-2">
+                        <div className="col-sm-6 col-md-4 col-lg-3 ">
+
+                            {/* Kalau mau ganti card, ganti dari bawah ini sampe div class card */}
+                            <div class="card" >
+                                <img src="https://www.olcbd.net/wp-content/uploads/2018/05/html-800x445.jpg" class="card-img-top" alt="img-course.jpg" />
+                                <div class="card-body">
+                                    <h5 class="card-title">HTML</h5>
+                                    <p>{/* Nama pembuat */} Dionisius <br />
+                                        <span class="card-text">4,6
+                                            <i class="fas fa-star"></i>
+                                            <i class="fas fa-star" ></i>
+                                            <i class="fas fa-star" ></i>
+                                            <i class="fas fa-star" ></i>
+                                            <i class="fas fa-star-half-alt" ></i>
+                                            <span> {/* Jumlah pembeli course */}(54)</span><br />
+                                        <span>Rp.250.000</span>
+                                    </span>
+                                    </p>
+
+                                    <a href="#" class="btn btn-primary">Detail Class</a>
+                                </div>
+
+                                    
+                            </div>
+                        </div>            
+
+                        <div className="col-sm-6 col-md-4 col-lg-3 ">
+                            <div class="card" >
+                                <img src="https://www.olcbd.net/wp-content/uploads/2018/05/html-800x445.jpg" class="card-img-top" alt="img-course.jpg" />
+                                <div class="card-body">
+                                    <h5 class="card-title">HTML</h5>
+                                    <p>{/* Nama pembuat */} Dionisius <br />
+                                        <span class="card-text">4,6
+                                            <i class="fas fa-star"></i>
+                                            <i class="fas fa-star" ></i>
+                                            <i class="fas fa-star" ></i>
+                                            <i class="fas fa-star" ></i>
+                                            <i class="fas fa-star-half-alt" ></i>
+                                            <span> {/* Jumlah pembeli course */}(54)</span><br />
+                                        <span>Rp.250.000</span>
+                                    </span>
+                                    </p>
+
+                                    <a href="#" class="btn btn-primary">Detail Class</a>
+                                </div>
+
+                                    
+                            </div>
+                        </div>    
+
+                        <div className="col-sm-6 col-md-4 col-lg-3 ">
+                            <div class="card" >
+                                <img src="https://www.olcbd.net/wp-content/uploads/2018/05/html-800x445.jpg" class="card-img-top" alt="img-course.jpg" />
+                                <div class="card-body">
+                                    <h5 class="card-title">HTML</h5>
+                                    <p>{/* Nama pembuat */} Dionisius <br />
+                                        <span class="card-text">4,6
+                                            <i class="fas fa-star"></i>
+                                            <i class="fas fa-star" ></i>
+                                            <i class="fas fa-star" ></i>
+                                            <i class="fas fa-star" ></i>
+                                            <i class="fas fa-star-half-alt" ></i>
+                                            <span> {/* Jumlah pembeli course */}(54)</span><br />
+                                        <span>Rp.250.000</span>
+                                    </span>
+                                    </p>
+
+                                    <a href="#" class="btn btn-primary">Detail Class</a>
+                                </div>
+
+                                    
+                            </div>
+                        </div> 
+
+                        <div className="col-sm-6 col-md-4 col-lg-3 ">
+                            <div class="card" >
+                                <img src="https://www.olcbd.net/wp-content/uploads/2018/05/html-800x445.jpg" class="card-img-top" alt="img-course.jpg" />
+                                <div class="card-body">
+                                    <h5 class="card-title">HTML</h5>
+                                    <p>{/* Nama pembuat */} Dionisius <br />
+                                        <span class="card-text">4,6
+                                            <i class="fas fa-star"></i>
+                                            <i class="fas fa-star" ></i>
+                                            <i class="fas fa-star" ></i>
+                                            <i class="fas fa-star" ></i>
+                                            <i class="fas fa-star-half-alt" ></i>
+                                            <span> {/* Jumlah pembeli course */}(54)</span><br />
+                                        <span>Rp.250.000</span>
+                                    </span>
+                                    </p>
+
+                                    <a href="#" class="btn btn-primary">Detail Class</a>
+                                </div>
+
+                                    
+                            </div>
+                        </div>                                                  
+
+                        <div className="col-sm-6 col-md-4 col-lg-3 ">
+                            <div class="card" >
+                                <img src="https://www.olcbd.net/wp-content/uploads/2018/05/html-800x445.jpg" class="card-img-top" alt="img-course.jpg" />
+                                <div class="card-body">
+                                    <h5 class="card-title">HTML</h5>
+                                    <p>{/* Nama pembuat */} Dionisius <br />
+                                        <span class="card-text">4,6
+                                            <i class="fas fa-star"></i>
+                                            <i class="fas fa-star" ></i>
+                                            <i class="fas fa-star" ></i>
+                                            <i class="fas fa-star" ></i>
+                                            <i class="fas fa-star-half-alt" ></i>
+                                            <span> {/* Jumlah pembeli course */}(54)</span><br />
+                                        <span>Rp.250.000</span>
+                                    </span>
+                                    </p>
+
+                                    <a href="#" class="btn btn-primary">Detail Class</a>
+                                </div>
+                            </div>
+                        </div>                      
+                        <div className="col-sm-6 col-md-4 col-lg-3 ">
+                            <div class="card" >
+                                <img src="https://www.olcbd.net/wp-content/uploads/2018/05/html-800x445.jpg" class="card-img-top" alt="img-course.jpg" />
+                                <div class="card-body">
+                                    <h5 class="card-title">HTML</h5>
+                                    <p>{/* Nama pembuat */} Dionisius <br />
+                                        <span class="card-text">4,6
+                                            <i class="fas fa-star"></i>
+                                            <i class="fas fa-star" ></i>
+                                            <i class="fas fa-star" ></i>
+                                            <i class="fas fa-star" ></i>
+                                            <i class="fas fa-star-half-alt" ></i>
+                                            <span> {/* Jumlah pembeli course */}(54)</span><br />
+                                        <span>Rp.250.000</span>
+                                    </span>
+                                    </p>
+
+                                    <a href="#" class="btn btn-primary">Detail Class</a>
+                                </div>        
+                            </div>
+                        </div>                                                              
+                        <div className="col-sm-6 col-md-4 col-lg-3 ">
+                            <div class="card" >
+                                <img src="https://www.olcbd.net/wp-content/uploads/2018/05/html-800x445.jpg" class="card-img-top" alt="img-course.jpg" />
+                                <div class="card-body">
+                                    <h5 class="card-title">HTML</h5>
+                                    <p>{/* Nama pembuat */} Dionisius <br />
+                                        <span class="card-text">4,6
+                                            <i class="fas fa-star"></i>
+                                            <i class="fas fa-star" ></i>
+                                            <i class="fas fa-star" ></i>
+                                            <i class="fas fa-star" ></i>
+                                            <i class="fas fa-star-half-alt" ></i>
+                                            <span> {/* Jumlah pembeli course */}(54)</span><br />
+                                        <span>Rp.250.000</span>
+                                    </span>
+                                    </p>
+
+                                    <a href="#" class="btn btn-primary">Detail Class</a>
+                                </div>
+
+                                    
+                            </div>
+                        </div>                                                                                                                
+                    </div>
+
+            </div>
+
+            {/* end class */}
+
+            {/* section testimoni */}
+
+            <div className="container mt-5 mb-5">
+                <center><h1>Testimoni</h1></center>
+
+                    <div className="row gy-3 mt-2 justify-content-evenly">
+                        
+                        <div className="col-sm-6 col-md-4 col-lg-3 ">
+
+                            {/* Kalau mau ganti card, ganti dari bawah ini sampe div class card */}
+                            <div class="card testi" >
+                                <img src="https://i.pinimg.com/564x/4b/0f/d9/4b0fd9c4f7fa6557d4681f150ade1e47.jpg" class="card-img-top" alt="img-course.jpg" />
+                                <div class="card-body">
+                                    <h5 class="card-title">Anri Daniata Purba</h5>
+                                    <p>{/* Nama pembuat */} <br />
+                                    <p>"Kelas ini sungguh luar biasa! Materi yang diajarkan sangat relevan dan guru memberikan penjelasan dengan sangat jelas. Saya merasa lebih percaya diri dalam memahami konsep-konsep yang sebelumnya sulit."</p>
+                                        <span class="card-text">
+                                            <i class="fas fa-star"></i>
+                                            <i class="fas fa-star" ></i>
+                                            <i class="fas fa-star" ></i>
+                                            <i class="fas fa-star" ></i>
+                                            <i class="fas fa-star-half-alt" ></i>
+                                        
+                                    </span>
+                                    </p>
+                                </div>
+                            </div>
+
+                            
+                        </div> 
+
+                        
+                            
+                        <div className="col-sm-6 col-md-4 col-lg-3 ">
+
+                            {/* Kalau mau ganti card, ganti dari bawah ini sampe div class card */}
+                            <div class="card testi" >
+                                <img src="https://i.pinimg.com/564x/7d/c3/0b/7dc30bfe7d1ca4234aaf4ab8d45666e9.jpg" class="card-img-top" alt="img-course.jpg" />
+                                <div class="card-body">
+                                    <h5 class="card-title">Muhammad Haikal Syahfarel</h5>
+                                    <p>{/* Nama pembuat */} <br />
+                                    <p>"Kelas website ini sungguh luar biasa! Materi yang disampaikan sangat jelas dan mudah dipahami. Saya benar-benar merasa meningkatkan keterampilan saya dalam membuat dan mengelola website. Terima kasih!"</p>
+                                        <span class="card-text">
+                                            <i class="fas fa-star"></i>
+                                            <i class="fas fa-star" ></i>
+                                            <i class="fas fa-star" ></i>
+                                            <i class="fas fa-star" ></i>
+                                            <i class="fas fa-star-half-alt" ></i>
+                                        
+                                    </span>
+                                    </p>
+                                </div>
+                            </div>
+
+
+                            
+                        </div> 
+
+                        <div className="col-sm-6 col-md-4 col-lg-3 align-center">
+
+                            {/* Kalau mau ganti card, ganti dari bawah ini sampe div class card */}
+                            <div class="card testi" >
+                                
+                                <img src="https://i.pinimg.com/564x/b6/4f/79/b64f798bbe07418db6abd047f49c3fa0.jpg" class="card-img-top" alt="img-course.jpg" />
+                                <div class="card-body">
+                                    <h5 class="card-title">Dionisius Reinaldo</h5>
+                                    <p>{/* Nama pembuat */} <br />
+                                    <p>"Saya sangat puas dengan kelas website ini. Instrukturnya sangat kompeten dan responsif terhadap pertanyaan. Materi yang diberikan sangat relevan dan praktis. Sangat direkomendasikan!"</p>
+                                        <span class="card-text">
+                                            <i class="fas fa-star"></i>
+                                            <i class="fas fa-star" ></i>
+                                            <i class="fas fa-star" ></i>
+                                            <i class="fas fa-star" ></i>
+                                            <i class="fas fa-star-half-alt" ></i>
+                                        
+                                    </span>
+                                    </p>
+                                </div>
+                            </div>
+
+
+                            
+                        </div> 
+
+                        
+                    </div>
+                    
+                    
+                </div>
+                
+  
+        </div>
+    )
+}
+
+export default DaftarCourse;
